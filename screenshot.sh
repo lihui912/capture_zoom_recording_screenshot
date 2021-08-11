67cbc3024b3a19db73a56a0ae8d99cee8d6f3a2d
chinlihui <lihui912@users.noreply.github.com>
2021-08-11 19:33:32 +0800

    Update screenshot.sh
    Add timestamp as filename prefix.

--- screenshot.sh
+++ screenshot.sh
@@ -6,6 +6,7 @@
 #########################
 
 PWD="$(pwd)"
+TIMESTAMP="$(date +%Y%m%d%H%M%S)"
 
 while getopts f: flag
 do
@@ -17,7 +18,7 @@ done
 echo "Filename: $filename";
 
 echo "Generating start.jpg"
-ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/screenshot_start.jpg -hide_banner -loglevel quiet
+ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/"${TIMESTAMP}"screenshot_start.jpg -hide_banner -loglevel quiet
 
 echo "Generating end.jpg"
-ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/screenshot_end.jpg -hide_banner -loglevel quiet
+ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/"${TIMESTAMP}"screenshot_end.jpg -hide_banner -loglevel quiet
