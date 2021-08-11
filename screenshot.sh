78c3bf7fde80021e88c5090072a38ef3275e4f10
chinlihui <lihui912@users.noreply.github.com>
2021-08-11 19:33:55 +0800

    Update screenshot.sh
    Quote the output path.

--- screenshot.sh
+++ screenshot.sh
@@ -18,7 +18,7 @@ done
 echo "Filename: $filename";
 
 echo "Generating start.jpg"
-ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/"${TIMESTAMP}"screenshot_start.jpg -hide_banner -loglevel quiet
+ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -vframes 1 "${PWD}"/"${TIMESTAMP}"screenshot_start.jpg -hide_banner -loglevel quiet
 
 echo "Generating end.jpg"
-ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -vframes 1 ${PWD}/"${TIMESTAMP}"screenshot_end.jpg -hide_banner -loglevel quiet
+ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -vframes 1 "${PWD}"/"${TIMESTAMP}"screenshot_end.jpg -hide_banner -loglevel quiet
