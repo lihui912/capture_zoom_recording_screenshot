d1d7db82ac1482343a64749e24f95710fc6fc9d6
chinlihui <lihui912@users.noreply.github.com>
2021-08-13 13:34:14 +0800

    Update screenshot.sh
    Update input file variable name.

--- screenshot.sh
+++ screenshot.sh
@@ -11,15 +11,15 @@ TIMESTAMP="$(date +%Y%m%d%H%M%S)"
 while getopts f:p: flag
 do
         case "${flag}" in
-                f) filename=${OPTARG};;
+                f) video_file=${OPTARG};;
                 p) prefix=${OPTARG};;
         esac
 done
 
-echo "Filename: $filename";
+echo "Input video file: $video_file"
 
 echo "Generating start.jpg"
-ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_start.jpg -hide_banner -loglevel quiet
+ffmpeg -y -ss 100 -i "${video_file}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_start.jpg -hide_banner -loglevel quiet
 
 echo "Generating end.jpg"
-ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_end.jpg -hide_banner -loglevel quiet
+ffmpeg -y -sseof -100 -i "${video_file}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_end.jpg -hide_banner -loglevel quiet
