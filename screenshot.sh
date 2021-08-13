0c043973502a87d015352b1bc05348e3087b65c3
chinlihui <lihui912@users.noreply.github.com>
2021-08-13 13:33:22 +0800

    Update screenshot.sh
    Add option for output filename prefix.

--- screenshot.sh
+++ screenshot.sh
@@ -8,17 +8,18 @@
 PWD="$(pwd)"
 TIMESTAMP="$(date +%Y%m%d%H%M%S)"
 
-while getopts f: flag
+while getopts f:p: flag
 do
         case "${flag}" in
                 f) filename=${OPTARG};;
+                p) prefix=${OPTARG};;
         esac
 done
 
 echo "Filename: $filename";
 
 echo "Generating start.jpg"
-ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${TIMESTAMP}"screenshot_start.jpg -hide_banner -loglevel quiet
+ffmpeg -y -ss 100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_start.jpg -hide_banner -loglevel quiet
 
 echo "Generating end.jpg"
-ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${TIMESTAMP}"screenshot_end.jpg -hide_banner -loglevel quiet
+ffmpeg -y -sseof -100 -i "${filename}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_end.jpg -hide_banner -loglevel quiet
