0432f241dc9933957b3e01481bd36d6df8fd0ba4
chinlihui <lihui912@users.noreply.github.com>
2021-08-19 08:30:49 +0800

    Add stdout after writing screenshot file.

--- screenshot.sh
+++ screenshot.sh
@@ -32,6 +32,15 @@ check_video_file_exists () {
         fi
 }
 
+print_execute_result () {
+        if [ $1 -eq 0 ];
+        then
+                echo "Written $2"
+        else
+                echo "Error $1. Writing $2 error."
+        fi
+}
+
 while getopts f:p: flag
 do
         case "${flag}" in
@@ -55,6 +64,8 @@ echo "Input video file: $video_file"
 
 echo "Generating ${prefix}_screenshot_start.jpg"
 ffmpeg -y -ss 100 -i "${video_file}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_start.jpg -hide_banner -loglevel quiet
+print_execute_result $? "${START_FILENAME}"
 
 echo "Generating ${prefix}_screenshot_end.jpg"
 ffmpeg -y -sseof -100 -i "${video_file}" -q:v 5 -frames:v 1 "${PWD}"/"${prefix}"_screenshot_end.jpg -hide_banner -loglevel quiet
+print_execute_result $? "${END_FILENAME}"
